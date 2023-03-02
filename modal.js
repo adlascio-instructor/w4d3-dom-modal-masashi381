3b09ffa5e1146df68cd1b308518d085bc3b20aaf
Masashi <ma.orange381@gmail.com>
2023-03-01 17:39:59 -0800

    w4d3-dom-modal-masashi381-solve

--- modal.js
+++ modal.js
@@ -1,9 +1,10 @@
 console.log("here");
-
+const btn = document.querySelector("#openModal");
+const show = document.querySelector("#modal");
 // select button element
-
+btn.addEventListener("click", () => {show.classList.remove("invisible")});
 // add event listener on click to button
 // remove class invisible from div modal
-
+show.addEventListener("click", () => {show.classList.add("invisible")});
 // add event listener on key press Escape
 // add class invisible to div modal
